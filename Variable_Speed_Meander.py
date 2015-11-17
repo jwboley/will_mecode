e05ec1ba01060ec4db3d7868c6de2fb982305516
alexvalentine <adominicv@gmail.com>
2015-11-17 15:38:29 -0500

    Version 1: Sweeps through print speeds.
    Sweeps through print speeds on the fly.

---
+++ Variable_Speed_Meander.py
@@ -0,0 +1,85 @@
+from mecode import G
+
+import numpy as np
+
+#Thanks Alex!!!
+
+#com_press = 16 #Mega com port for pressure (Make sure you verify!)
+#pressure = 40
+#vps_high = 10 #High speed velocity to move to initial print location
+#vps = 5 #Operating print speed
+v = [1.0,0.1,0.01,0.001]
+h0_high = 0.06 #Nozzle offset for high speed motion to initial print location
+h0 = 0 #Operating nozzle offset
+pdwell = 1 #Dwell time for fluid despensing at start of print job
+
+g = G(
+      direct_write = False,
+      outfile = r'\\seasfs02.rc.fas.harvard.edu\jlewis_lab\User Files\Boley\Print_Routines\Variable_Speed_Meander.pgm',
+      header = r'C:\Users\Aerosol Jet\Documents\GitHub\will_mecode\mymegacasterheader.txt',
+      footer = r'C:\Users\Aerosol Jet\Documents\GitHub\will_mecode\mymegacasterfooter.txt',
+      print_lines = False,
+      aerotech_include=False,
+      )
+      
+#Remember to zero at lower left corner via 'G92 X0 Y0 Z0'
+
+margin = 8
+xl = 2*2.54*10 #2" by 3" slide
+yl = 3*2.54*10 #2" by 3" slide
+
+
+
+x0 = 0
+y0 = 2*margin+yl
+# 
+
+#####glass slide border used for print path troubleshooting######
+#g.move(x=2*2.54*10) 
+#g.move(y=3*2.54*10)
+#g.move(x=-2*2.54*10)
+#g.move(y=-3*2.54*10)
+#################################################################
+
+#g.set_pressure(com_press, pressure)
+#g.feed(vps_high)
+#g.abs_move(z = h0_high)
+#g.abs_move(x = x0, y = yl-margin)
+#g.abs_move(z = h0)
+#g.toggle_pressure(com_press)
+#g.dwell(pdwell)
+  
+  
+n = 4 #number of lines for each unit meander  
+def unit_meander(n,xl,p,v):
+    g.feed(v)
+    for i in range(n):
+        if i%2 == 0:
+            c = 'CW'
+            g.abs_move(x = xl - margin)
+            g.arc(x = 0, y = -p, radius = p/2, direction = c)
+        else:
+            c = 'CCW'
+            g.abs_move(x = margin)
+            g.arc(x = 0, y = -p, radius = p/2, direction = c)
+
+
+p0 = 4 #pitch for first unit meander
+pN = 0.2 #pitch for Nth unit meander
+r = ((yl-margin)-n*p0)/((yl-margin)-n*pN) #factor reduction for spacing of subsequent meanders
+N = int(np.rint(np.log(pN/p0)/np.log(r))) #total number of unit meanders
+p0 = pN/np.power(r,N) #redefine p0 to account for rounding error
+
+def meta_meander(xl,n,v,p0):
+    for i in range(len(v)):
+        unit_meander(n=n,xl=xl,p=p0,v=v[i])
+        
+
+#g.toggle_pressure(com_press)
+#g.feed(10)
+#g.clip(height=2, direction='-x')
+
+
+meta_meander(xl=xl,n=n,v=v,p0=p0) 
+g.view(backend = 'matplotlib')#plot print path
+g.teardown()#ends the script (never comment out)

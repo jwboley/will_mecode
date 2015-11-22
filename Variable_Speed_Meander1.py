f69616cdb32a198cad7b7d4b60f25448fd316239
alexvalentine <adominicv@gmail.com>
2015-11-22 16:54:00 -0500

    Revision
    Minor Revision

--- Variable_Speed_Meander1.py
+++ Variable_Speed_Meander1.py
@@ -4,13 +4,13 @@ import numpy as np
 g = G(
       direct_write = False,
       ### When Editing On Megacaster ###
-      #outfile = r'\\seasfs02.rc.fas.harvard.edu\jlewis_lab\User Files\Boley\Print_Routines\Variable_Speed_Meander.pgm',
-      #header = r'C:\Users\Aerosol Jet\Documents\GitHub\will_mecode\mymegacasterheader.txt',
-      #footer = r'C:\Users\Aerosol Jet\Documents\GitHub\will_mecode\mymegacasterfooter.txt',
+      outfile = r'\\seasfs02.rc.fas.harvard.edu\jlewis_lab\User Files\Boley\Print_Routines\Variable_Speed_Meander.pgm',
+      header = r'C:\Users\Aerosol Jet\Documents\GitHub\will_mecode\mymegacasterheader.txt',
+      footer = r'C:\Users\Aerosol Jet\Documents\GitHub\will_mecode\mymegacasterfooter.txt',
       ### When Editing On Laptop ###
-      outfile = r'/Users/jwboley/Documents/will_mecode/Variable_Speed_Meander.pgm',
-      header = r'/Users/jwboley/Documents/will_mecode/mymegacasterheader.txt',
-      footer = r'/Users/jwboley/Documents/will_mecode/mymegacasterfooter.txt',
+      #outfile = r'/Users/jwboley/Documents/will_mecode/Variable_Speed_Meander.pgm',
+      #header = r'/Users/jwboley/Documents/will_mecode/mymegacasterheader.txt',
+      #footer = r'/Users/jwboley/Documents/will_mecode/mymegacasterfooter.txt',
       print_lines = False,
       aerotech_include=False,
       )
@@ -46,7 +46,7 @@ def unit_meander(n,xl,p,v,ctr,q):
             g.arc(x = 0, y = -p, radius = p/2, direction = c)
         i += 1
         
-p = 3.0 #pitch between meanders in mm
+p = 4.0 #pitch between meanders in mm
 N = 4 #total number of unit meanders
 d = 0.2 #inner diameter of nozzle in mm
 v0 = 10 #speed for first unit meander in mm/s
